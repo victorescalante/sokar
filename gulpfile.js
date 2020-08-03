7dcf6b8c932fce27cff76b6b669fdfbaa4aa18ca
Victor Escalante <victorlt.xd3@gmail.com>
2020-08-02 23:42:48 -0500

    check deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var ftp = require('vinyl-ftp');
+var gulp = require('gulp');
 var gutil = require('gulp-util');
 var minimist = require('minimist');
 var args = minimist(process.argv.slice(2));
 
 gulp.task('deploy', function() {
-  var remotePath = '/public_html/';
-  var publicPath = 'dist';
+  var remotePath = 'dist';
+  var publicPath = '/public_html/';
   var conn = ftp.create({
     host: '192.232.238.157',
     user: args.user,
@@ -13,7 +14,7 @@ gulp.task('deploy', function() {
     log: gutil.log
   });
 
-gulp.src(['index.html', './**/*.css'])
+gulp.src(['dist/**'], {buffer: false})
   .pipe(conn.newer(remotePath))
   .pipe(conn.dest(publicPath));
 });
